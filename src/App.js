59373ba4b1bae6bab3a92e70754f2ae2201f300e
Robbie R <git@robbie.computer>
2021-11-06 15:31:23 +0000

    Complete connect wallet button lesson

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
@@ -6,14 +7,74 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+          const publicKey = response.publicKey.toString();
+          console.log('Connected with Public Key:', publicKey);
+
+          // Set publicKey in state
+          setWalletAddress(publicKey);
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  /*
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+   */
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      const publicKey = response.publicKey.toString();
+      console.log('Connected with Public Key:', publicKey);
+      setWalletAddress(publicKey);
+    }
+  };
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+   */
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button" onClick={connectWallet}>
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    window.addEventListener('load', async (e) => {
+      await checkIfWalletIsConnected();
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
-          <p className="sub-text">
-            View your GIF collection in the metaverse ✨
-          </p>
+          <p className="sub-text">View your GIF collection in the metaverse ✨</p>
+          {/* Render your connect to wallet button right here */}
+          <div className={walletAddress ? 'authed-container' : 'container'}>
+            {!walletAddress && renderNotConnectedContainer()}
+          </div>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
